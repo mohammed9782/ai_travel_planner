d6ed29e4adb216c38f4db386b603c18b333f6edb
Mohammed <mahfoudmohammed01@gmail.com>
2026-03-20 18:23:30 +0100

    Add Ollama model integration and initial project structure with README and main entry point

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from agno.run.agent import RunOutput
 from agno.tools.mcp import MultiMCPTools
 from agno.tools.googlesearch import GoogleSearchTools
 from agno.models.openai import OpenAIChat
+from agno.models.ollama import Ollama
 from icalendar import Calendar, Event
 from datetime import datetime, timedelta
 import streamlit as st
@@ -90,7 +91,8 @@ async def run_mcp_travel_planner(destination: str, num_days: int, preferences: s
         travel_planner = Agent(
             name="Travel Planner",
             role="Creates travel itineraries using Airbnb, Google Maps, and Google Search",
-            model=OpenAIChat(id="minimax/minimax-m2.5:free", api_key=openai_key, base_url='https://openrouter.ai/api/v1',),
+            model=Ollama(id="qwen2.5:3b"), 
+            # model=OpenAIChat(id="minimax/minimax-m2.5:free", api_key=openai_key, base_url='https://openrouter.ai/api/v1',),
             description=dedent(
                 """\
                 You are a professional travel consultant AI that creates highly detailed travel itineraries directly without asking questions.
